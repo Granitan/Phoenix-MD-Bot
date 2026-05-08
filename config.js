07d8ff8c98c341f8ebe7a894e57238959f972285
Granitan <lolagatop@gmail.com>
2026-05-08 13:44:38 +0200

    Change hardcoded SESSION_ID in config.js
    Updated SESSION_ID to a new hardcoded value.

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
  // ℹ️ Removed commands like welcome,goodbye etc and other commands and features that cause whatsapp account/group ban thank you!
 //____________________________________________________________________________________________________________________________________________  
   //🤖 BOT CONFIGURATION
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || '2@n1KoNcMaR9l+s92vx2Lr0nNasm7kPdrMtm4JQOAIaHiICATuUC6AuJi2Uf/REI0y92TNWec1Fn6pVBzFlMqlJRHI1DNRz36ZIgs=,8Vx0tWMH8dfMh7FLj/2NwXXUM5ZNuP2JPoUYwnOtDAs=,p9Jbg2yt7fejTyxeZtryweaQjavyK9c/0nYI+KrzBUQ=,urWnT8RY1f0cCUwAGIVSIRbmJlOx55GaDs6gURHylRU=',
   STICKER_DATA: process.env.STICKER_DATA || '🎯ᴘʜᴏᴇɴɪx-ᴍᴅ;ᴀʙʜɪꜱʜᴇᴋ ꜱᴜʀᴇꜱʜ🍀',
   ALIVE_DATA: process.env.ALIVE_DATA || '👋 ʜᴇʏ &sender, ɪ ᴍ *ᴘʜᴏᴇɴɪx-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ* ᴀʟɪᴠᴇ ɴᴏᴡ!\n\n📌 ᴛʏᴘᴇ *menu* ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.\n\n*ᴘʟᴀᴛꜰᴏʀᴍ:* &platform\n*ʀᴜɴᴛɪᴍᴇ:* &runtime;https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg',
   AUDIO_DATA: process.env.AUDIO_DATA || 'Phoenix-MD;Abhishek Suresh;https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg',
